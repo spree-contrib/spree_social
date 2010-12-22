2dfd7dceeb82a47d3b0351c66ff5ae676fe78205
Sean Schofield <sean@railsdog.com>
2010-12-22 14:25:46 -0500

    Do not show social fail screen for standard validation/update problems.

--- app/controllers/users_controller_decorator.rb
+++ app/controllers/users_controller_decorator.rb
@@ -1,13 +1,17 @@
 UsersController.class_eval do
-  
+
   update do
-    failure do 
+    failure do
       flash "There is already an account with that email. Please sign in to associate these accounts."
       wants.html do
-        render(:template => 'users/merge')
+        if @user.anonymous?
+          render(:template => 'users/merge')
+        else
+          redirect_to edit_object_path
+        end
       end
     end
   end
   update.flash { I18n.t("account_updated") }
-  
+
 end
