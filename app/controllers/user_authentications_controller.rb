1cce3cc0db501c989f3455d10994a2a0fbeba616
John Brien <iam@hybridindie.com>
2010-12-23 10:10:54 -0500

    created self-contained authentication source updating

--- app/controllers/user_authentications_controller.rb
+++ app/controllers/user_authentications_controller.rb
@@ -1,4 +1,16 @@
-class UserAuthenticationsController < ApplicationController
+class UserAuthenticationsController < Spree::BaseController
+  
+  def update
+    @user = User.find(params[:id])
+    @user.email = params[:user][:email]
+    if @user.save
+      redirect_back_or_default(product_path)
+    else
+      flash[:alert] = "There is already an account with that email. Please sign in to associate these accounts."
+      render(:template => 'users/merge')
+    end
+  end
+  
   def destroy
     @auth = current_user.user_authentications.find(params[:id])
     @auth.destroy
