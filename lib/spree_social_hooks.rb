a774f3afb35156fba72517f69de757902734b300
Brian Quinn <briandquinn@gmail.com>
2011-05-31 12:45:05 +0100

    Updated to use Deface

--- lib/spree_social_hooks.rb
+++
@@ -1,7 +0,0 @@
-class SocialHooks < Spree::ThemeSupport::HookListener
-  replace :account_summary , 'users/social'
-  insert_after :login_extras, 'shared/socials'
-  insert_after :admin_configurations_sidebar_menu, 'admin/shared/sidebar_menu'
-  insert_after :admin_configurations_menu, 'admin/shared/configurations_menu'
-  
-end
