0a008cf49f34cc0c0b06a21e1c08c325cc85328d
John Brien <iam@hybridindie.com>
2011-08-09 13:03:46 -0400

    fixes #13 with a work-around

--- lib/spree_social.rb
+++ lib/spree_social.rb
@@ -20,9 +20,9 @@ module SpreeSocial
     ["Yahoo!", "yahoo"], 
     ["YouTube", "you_tube"], 
     ["Vkontakte", "vkontakte"], 
-    ["Mailru", "mailru"],
-    ["Blogger", "blogger"],
-    ["Dropbox", "dropbox"]
+    ["Mailru", "mailru"]
+    #["Blogger", "blogger"],
+    #["Dropbox", "dropbox"]
   ]
 
   class Engine < Rails::Engine
@@ -43,7 +43,7 @@ module SpreeSocial
       if user.preferred_provider == provider
         key = user.preferred_api_key
         secret = user.preferred_api_secret
-        puts("[Spree Social] Loading #{user.preferred_provider.capitalize} as authentication source")
+        ::Rails.logger.info("[Spree Social] Loading #{user.preferred_provider.capitalize} as authentication source")
       end
     end if self.table_exists?("authentication_methods") # See Below for explanation
     self.setup_key_for(provider.to_sym, key, secret)
--- spree_social.gemspec
+++ spree_social.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_social'
-  s.version     = '1.2.1'
+  s.version     = '1.3'
   s.summary     = 'Adds social network login services (OAuth) to Spree'
   #s.description = 'Add (optional) gem description here'
   s.required_ruby_version = '>= 1.8.7'
@@ -14,8 +14,8 @@ Gem::Specification.new do |s|
   s.require_path = 'lib'
   s.requirements << 'none'
 
-  s.add_dependency('spree_core', '>= 0.60.0.RC1')
-  s.add_dependency('spree_auth', '>= 0.60.0.RC1')
-  s.add_dependency('oa-oauth', '>= 0.2.2') # we don't need all of the omniauth family just the oAuth stuff'
+  #s.add_dependency('spree_core', '>= 0.60.0.RC1')
+  s.add_dependency('spree_auth', '>= 0.60.1')
+  s.add_dependency('oa-oauth', ">= 0.2.6") # we don't need all of the omniauth family just the oAuth stuff'
 
 end
