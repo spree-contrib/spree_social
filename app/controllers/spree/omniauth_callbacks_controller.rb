8c3085687d6999b963392a388f618b86ce9f0a01
John Dyer <lbrapid@gmail.com>
2012-01-20 15:59:16 -0500

    Handle logged out users as well

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -1,11 +1,25 @@
 class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     auth = request.env["omniauth.auth"]
-    #@user_auth = UserAuthentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
-
-    current_user.user_authentications.create(:provider => auth['provider'], :uid => auth['uid'])
-    flash[:notice] = "Auth successful!"
-    redirect_to user_authentications_path
+    authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
+    if authentication
+      flash[:notice] = "Signed in successfully"
+      sign_in_and_redirect :user, authentication.user
+    elsif current_user
+      current_user.user_authentications.create!(:provider => auth['provider'], :uid => auth['uid'])
+      flash[:notice] = "Authentication successful."
+      redirect_to user_authentications_url
+    else
+      user = Spree::User.new
+      user.apply_omniauth(auth)
+      if user.save
+        flash[:notice] = "Signed in successfully."
+        sign_in_and_redirect :user, user
+      else
+        session[:omniauth] = auth.except('extra')
+        redirect_to spree.new_user_registration_url
+      end
+    end
   end
 
   def failure
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,4 +2,8 @@ Spree::User.class_eval do
   has_many :user_authentications
 
   devise :omniauthable, :omniauth_providers => [:twitter]
+
+  def apply_omniauth(omniauth)
+    user_authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
+  end
 end
