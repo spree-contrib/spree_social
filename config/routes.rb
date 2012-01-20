11c16750b8ed57b89e8ea14f493ce54f17138aa4
John Dyer <lbrapid@gmail.com>
2012-01-20 15:59:45 -0500

    Specify registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Spree::Core::Engine.routes.append do
   devise_for :users,
              :class_name => Spree::User,
-             :skip => [:registrations, :unlocks],
-             :controllers => { :sessions => 'spree/user_sessions', :omniauth_callbacks => "spree/omniauth_callbacks" }
+             :skip => [:unlocks],
+             :controllers => { :sessions => 'spree/user_sessions', :omniauth_callbacks => "spree/omniauth_callbacks", :registrations => 'spree/user_registrations' }
   resources :user_authentications
 
   match 'account' => 'users#show', :as => 'user_root'
