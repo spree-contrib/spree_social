dbd1002fc619b730da8c71e0b95a4f95d0e8259a
John Dyer <lbrapid@gmail.com>
2012-01-20 15:59:34 -0500

    Show connected social networks in account summary

---
+++ app/overrides/add_authentications_to_account_summary.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/users/show",
+                     :name => "add_socials_to_account_summary",
+                     :insert_after => "[data-hook='account_my_orders']",
+                     :partial => 'spree/shared/social',
+                     :disabled => false)
