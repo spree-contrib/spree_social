e8bd89e81c7552ef3f71d756bc12c634257500d2
John Dyer <lbrapid@gmail.com>
2012-01-20 15:32:45 -0500

    Validate the model when returning from an oauth callback if a new spree user is required

---
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -0,0 +1,11 @@
+Spree::UserRegistrationsController.class_eval do
+  private
+
+  def build_resource(*args)
+    super
+    if session[:omniauth]
+      @user.apply_omniauth(session[:omniauth])
+      @user.valid?
+    end
+  end
+end
