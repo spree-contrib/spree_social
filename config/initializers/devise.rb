686a24c951a2bb1ae3a1c47680599006abc15234
John Dyer <lbrapid@gmail.com>
2012-01-24 12:47:19 -0500

    Fixes rake test_app

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+require 'devise'
+
 Devise.setup do |config|
   SpreeSocial::OAUTH_PROVIDERS.each do |provider|
     SpreeSocial.init_provider(provider[1])
--- lib/spree_social.rb
+++ lib/spree_social.rb
@@ -1,4 +1,5 @@
 require 'spree_core'
+require 'spree_auth'
 require 'omniauth-twitter'
 require 'omniauth-facebook'
 require 'spree_social/engine'
