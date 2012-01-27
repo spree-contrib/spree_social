fd62d0025e50cfc1f59b2ae27b6adcf8a521d02e
John Dyer <lbrapid@gmail.com>
2012-01-27 10:19:41 -0500

    Better handling of new user signup through oauth

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -29,6 +29,7 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
               sign_in_and_redirect :user, user
             else
               session[:omniauth] = auth_hash.except('extra')
+              flash[:notice] = t(:one_more_step, :kind => auth_hash['provider'].capitalize)
               redirect_to new_user_registration_url
             end
           end
