b2b9471013fd13fd6c270856f2ff02cb28245121
Brian Quinn <briandquinn@gmail.com>
2012-02-09 16:20:28 +0000

    Check for preferences that can be migrated

--- db/migrate/20111213154044_namespace_spree_social_tables.rb
+++ db/migrate/20111213154044_namespace_spree_social_tables.rb
@@ -1,6 +1,10 @@
+require 'spree/core/preference_rescue'
+
 class NamespaceSpreeSocialTables < ActiveRecord::Migration
   def change
     rename_table :user_authentications,   :spree_user_authentications
     rename_table :authentication_methods, :spree_authentication_methods
+
+    Spree::PreferenceRescue.try
   end
 end
