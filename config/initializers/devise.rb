e4541cc93426a491f9902ee2cb457f1d2e590644
John Dyer <lbrapid@gmail.com>
2012-03-16 14:26:39 -0400

    Clean up initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,3 @@
-require 'devise'
-
-Devise.setup do |config|
-  SpreeSocial::OAUTH_PROVIDERS.each do |provider|
-    SpreeSocial.init_provider(provider[1])
-  end
+SpreeSocial::OAUTH_PROVIDERS.each do |provider|
+  SpreeSocial.init_provider(provider[1])
 end
