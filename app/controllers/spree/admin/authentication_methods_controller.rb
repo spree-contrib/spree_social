a096c73b4742ec8d660723e9a2a119e7a95f5f53
John Dyer <lbrapid@gmail.com>
2012-03-16 14:32:45 -0400

    No need to restart the application after configuring or updating an authentication method

--- app/controllers/spree/admin/authentication_methods_controller.rb
+++ app/controllers/spree/admin/authentication_methods_controller.rb
@@ -1,7 +1,19 @@
 module Spree
   module Admin
     class AuthenticationMethodsController < ResourceController
+      create.after :update_oauth_method
+      update.after :update_oauth_method
 
+      private
+
+      def update_oauth_method
+        auth_method = params[:authentication_method]
+        if auth_method[:active] == "true" && auth_method[:environment] == ::Rails.env
+          Devise.setup do |config|
+            config.omniauth auth_method[:provider], auth_method[:api_key], auth_method[:api_secret]
+          end
+        end
+      end
     end
   end
 end
