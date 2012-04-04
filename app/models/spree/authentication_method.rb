81671094b4fca2079dcd6d8ffb7112ee9fc98641
Phill Campbell <phillcampbell@me.com>
2012-04-04 18:47:00 +0100

    Changed method name from camelcase to snakecase

--- app/models/spree/authentication_method.rb
+++ app/models/spree/authentication_method.rb
@@ -1,7 +1,7 @@
 class Spree::AuthenticationMethod < ActiveRecord::Base
   attr_accessible :provider, :api_key, :api_secret, :environment, :active
 
-  def self.activeAuthenticationMethods?
+  def self.active_authentication_methods?
     found = false
     where(:environment => ::Rails.env).each do |method|
       if method.active
