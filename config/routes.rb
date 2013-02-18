71e7b85e498aad5967f1579749841f312ff09b11
Jack Kinsella <jack.kinsella@gmail.com>
2013-02-18 18:35:18 +0100

    trying out mahinders solution to issue 62

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Spree::Core::Engine.routes.append do
   devise_for :users,
              :class_name => Spree::User,
              :skip => [:unlocks],
-             :controllers => { :sessions => 'spree/user_sessions', :omniauth_callbacks => "spree/omniauth_callbacks", :registrations => 'spree/user_registrations' } do 
-             	get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
-    			get '/users/auth/:provider/setup' => 'users/omniauth_callbacks#setup'
-             end
+             :controllers => { :sessions => 'spree/user_sessions', :omniauth_callbacks => "spree/omniauth_callbacks", :registrations => 'spree/user_registrations' }
   resources :user_authentications
 
   match 'account' => 'users#show', :as => 'user_root'
