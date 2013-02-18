547f5de611dee96b5d771df431ad3913235a5c05
Jack Kinsella <jack.kinsella@gmail.com>
2013-02-18 18:35:18 +0100

    Correctly namespace routes for configuration menus
    Fixes #67

--- app/overrides/admin_configuration_decorator.rb
+++ app/overrides/admin_configuration_decorator.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "add_social_providers_link_configuration_menu",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text => %q{<%= configurations_sidebar_menu_item t("social_authentication_methods"), admin_authentication_methods_path %>},
+                     :text => %q{<%= configurations_sidebar_menu_item t("social_authentication_methods"), spree.admin_authentication_methods_path %>},
                      :disabled => false)
 
 Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
