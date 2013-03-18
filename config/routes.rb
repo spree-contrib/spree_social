28c4ca30d1f0e2b50ed2b3429f07f34f014fc16e
Brian Buchalter <brianb@endpoint.com>
2013-03-18 17:33:17 -0300

    Allow users to specify user_root_path
    For issue #34

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Spree::Core::Engine.routes.append do
              :controllers => { :sessions => 'spree/user_sessions', :omniauth_callbacks => "spree/omniauth_callbacks", :registrations => 'spree/user_registrations' }
   resources :user_authentications
 
-  match 'account' => 'users#show', :as => 'user_root'
+  # Please define a user_root named path if you'd like to have
+  # logins redirect to something other than root_path or session["user_redirect_to"]
+  # match 'account' => 'users#show', :as => 'user_root'
 
   namespace :admin do
     resources :authentication_methods
