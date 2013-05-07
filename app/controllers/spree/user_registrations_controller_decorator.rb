a2e00f49993b4b0c42d8ccbf54618b0383a0dc3a
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-05-07 18:23:53 -0400

    Fix user references to spree_user.

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -7,12 +7,12 @@ Spree::UserRegistrationsController.class_eval do
   def build_resource(*args)
     super
     if session[:omniauth]
-      @user.apply_omniauth(session[:omniauth])
+      @spree_user.apply_omniauth(session[:omniauth])
     end
-    @user
+    @spree_user
   end
 
   def clear_omniauth
-    session[:omniauth] = nil unless @user.new_record?
+    session[:omniauth] = nil unless @spree_user.new_record?
   end
 end
