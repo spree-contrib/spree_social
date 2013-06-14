5e6a7534f94122ee85c6ba1debe4bef0e469167e
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-06-14 01:14:43 -0400

    Change how password required is called & setup selenium.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../dummy/config/environment', __FILE__)
 require 'rspec/rails'
 require 'spree/testing_support/url_helpers'
+require 'database_cleaner'
 require 'ffaker'
 
 # Requires supporting files with custom matchers and macros, etc,
@@ -25,13 +26,31 @@ RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
+  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
+  # to setup a test will be unavailable to the browser, which runs under a seperate server instance.
+  config.use_transactional_fixtures = false
+
+  # Ensure Suite is set to use transactions for speed.
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  # Before each spec check if it is a Javascript test and switch between using database transactions or not where necessary.
+  config.before :each do
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  # After each spec clean the database.
+  config.after :each do
+    DatabaseCleaner.clean
+  end
 
   config.include Spree::TestingSupport::UrlHelpers
   config.include Spree::TestingSupport::ControllerRequests, :type => :controller
   config.include Devise::TestHelpers, :type => :controller
+  config.include FactoryGirl::Syntax::Methods
   config.include Rack::Test::Methods, :type => :requests
 end
