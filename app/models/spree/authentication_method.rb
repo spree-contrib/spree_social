076c606eca865ae4db7062f97120334edf905d93
Ryan Bigg <radarlistener@gmail.com>
2013-07-04 10:33:30 +1000

    Simplify AuthenticationMethod#active_authentication_methods?

--- app/models/spree/authentication_method.rb
+++ app/models/spree/authentication_method.rb
@@ -2,13 +2,7 @@ class Spree::AuthenticationMethod < ActiveRecord::Base
   attr_accessible :provider, :api_key, :api_secret, :environment, :active
 
   def self.active_authentication_methods?
-    found = false
-    where(:environment => ::Rails.env).each do |method|
-      if method.active
-        found = true
-      end
-    end
-    return found
+    where(:environment => ::Rails.env, :active => true).exists?
   end
 
   scope :available_for, lambda { |user|
