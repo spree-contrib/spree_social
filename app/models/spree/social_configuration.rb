cca33a2fcb679da2120fc910e470ca7faa5ec697
Surya Tripathi <raj.surya19@gmail.com>
2013-07-12 12:12:56 +0530

    changed preference for path from spree_user to user

--- app/models/spree/social_configuration.rb
+++ app/models/spree/social_configuration.rb
@@ -1,6 +1,6 @@
 # More on Spree's preference configuration - http://guides.spreecommerce.com/preferences.html#site_wide_preferences
 module Spree
   class SocialConfiguration < Preferences::Configuration
-    preference :path_prefix, :string, :default => 'spree_user'
+    preference :path_prefix, :string, :default => 'user'
   end
 end
