839fe22611df2dffdf34c8df6480b4f90fa5f698
Stefan Wrobel <swrobel@gmail.com>
2013-08-15 17:28:03 -0700

    Update path_prefix default to match the old default
    The old route was /users/auth/:provider and this has changed the default to /user/auth/:provider
    I believe the default should not break existing apps that were relying on that route
    
    Fixes #103

--- app/models/spree/social_configuration.rb
+++ app/models/spree/social_configuration.rb
@@ -1,6 +1,6 @@
 # More on Spree's preference configuration - http://guides.spreecommerce.com/preferences.html#site_wide_preferences
 module Spree
   class SocialConfiguration < Preferences::Configuration
-    preference :path_prefix, :string, :default => 'user'
+    preference :path_prefix, :string, :default => 'users'
   end
 end
