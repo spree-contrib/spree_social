ee5805a8c45d84fe28160d84e2625f54747d2598
Ryan Bigg <radarlistener@gmail.com>
2013-09-19 12:33:16 +1000

    Define routes in a Rails 4 friendly way

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
-Spree::Core::Engine.routes.append do
+Spree::Core::Engine.routes.draw do
   devise_for :spree_user,
              :class_name => Spree::User,
-             :skip => [:unlocks],
-             :controllers => { :sessions => 'spree/user_sessions', :omniauth_callbacks => "spree/omniauth_callbacks", :registrations => 'spree/user_registrations' },
+             :skip => [:unlocks, :sessions, :registrations],
+             :controllers => { :omniauth_callbacks => "spree/omniauth_callbacks" },
              :path => Spree::SocialConfig[:path_prefix]
   resources :user_authentications
 
-  match 'account' => 'users#show', :as => 'user_root'
+  get 'account' => 'users#show', :as => 'user_root'
 
   namespace :admin do
     resources :authentication_methods
