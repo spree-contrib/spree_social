5b1f08e4fdc20acc6d0743c87b2b7714cedb549b
Tobias Bohwalli <hi@futhr.io>
2014-02-15 19:49:12 +0100

    Change default img icons to responsive font icons
    Using Entypo fonts by Daniel Bruce. Licenced under CC BY-SA. Fonts are
    embedded for minimal approach and only include fonts for current
    providers. Tweaked icon names to avoid collision.
    
    Conflicts:
    	app/assets/stylesheets/spree/frontend/spree_social.css

---
+++ app/assets/javascripts/spree/frontend/spree_social.js
@@ -0,0 +1,1 @@
+// Placeholder for specs dummy app
--- lib/generators/spree_social/install/install_generator.rb
+++ lib/generators/spree_social/install/install_generator.rb
@@ -3,6 +3,10 @@ module SpreeSocial
     class InstallGenerator < Rails::Generators::Base
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_social\n", before: /\*\//, verbose: true
+      end
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_social'
       end
