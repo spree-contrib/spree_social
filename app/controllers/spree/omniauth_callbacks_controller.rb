47ada210be9905a24f338c0164314a02431a744f
Justin McNally <justin@kohactive.com>
2014-07-14 13:49:00 -0500

    ensure user even if auth... unlikely but best to gaurd against it

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -17,7 +17,7 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
           authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
 
-          if authentication.present?
+          if authentication.try(:user).present? and authentication.present?
             flash[:notice] = t("devise.omniauth_callbacks.success", :kind => auth_hash['provider'])
             sign_in_and_redirect :spree_user, authentication.user
           elsif spree_current_user
