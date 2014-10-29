b8ec6bcc395c940ed41d696ddbfe8aef528f3723
Matt Dunbar <matt@buildrx.com>
2014-10-29 16:26:21 -0400

    Added support for login with amazon and added workaround for devise routing limitation (see issue #113) fixes #142

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,3 +4,10 @@ end
 
 OmniAuth.config.logger = Logger.new(STDOUT)
 OmniAuth.logger.progname = "omniauth"
+
+OmniAuth.config.on_failure = Proc.new do |env|
+  env['devise.mapping'] = Devise.mappings[Spree.user_class.table_name.singularize.to_sym]
+  controller_name  = ActiveSupport::Inflector.camelize(env['devise.mapping'].controllers[:omniauth_callbacks])
+  controller_klass = ActiveSupport::Inflector.constantize("#{controller_name}Controller")
+  controller_klass.action(:failure).call(env)
+end
