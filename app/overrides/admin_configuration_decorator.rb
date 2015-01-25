c0eee5c3a09c3edc9d14cd78596b29f46d289762
Tobias Bohwalli <hi@futhr.io>
2015-01-25 11:05:31 +0100

    Remove Deface option "disabled".
    Disabled option is unnecessary and may just raise deprecation warnings.

--- app/overrides/admin_configuration_decorator.rb
+++ app/overrides/admin_configuration_decorator.rb
@@ -1,5 +1,6 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-                     name: 'add_social_providers_link_configuration_menu',
-                     insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-                     text: '<%= configurations_sidebar_menu_item Spree.t(:social_authentication_methods), spree.admin_authentication_methods_path %>',
-                     disabled: false)
+Deface::Override.new(
+  virtual_path: 'spree/admin/shared/sub_menu/_configuration',
+  name: 'add_social_providers_link_configuration_menu',
+  insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
+  text: '<%= configurations_sidebar_menu_item Spree.t(:social_authentication_methods), spree.admin_authentication_methods_path %>'
+)
