8188457e908017e87a4972adf6a3124df6ab5785
Tobias Bohwalli <hi@futhr.io>
2015-02-12 06:14:43 +0100

    Fixes spree spec support.

--- spec/support/spree.rb
+++ spec/support/spree.rb
@@ -5,6 +5,6 @@ require 'spree/testing_support/url_helpers'
 require 'spree/testing_support/capybara_ext'
 
 RSpec.configure do |config|
-  config.include Spree::TestingSupport::ControllerRequests
+  config.include Spree::TestingSupport::ControllerRequests, type: :controller
   config.include Spree::TestingSupport::UrlHelpers
 end
