adf409a7880c7382c76fcf81df5c010d41f5bada
Nitin Gupta <nitinguptawebdev@gmail.com>
2016-03-12 17:39:24 +0530

    Fix test cases

--- spec/lib/spree_social/engine_spec.rb
+++ spec/lib/spree_social/engine_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe SpreeSocial do
         %w(Twitter twitter false),
         %w(Github github false),
         %w(Google google_oauth2 true)
+        %w(Paypal paypal true)
       ]
       expect(described_class::OAUTH_PROVIDERS).to match_array oauth_providers
     end
