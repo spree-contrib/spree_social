17d83ae2dab43babaaed6f0191d11977118dd54c
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2017-09-23 00:39:31 +0200

    use dirty hack to skip setting up providers without db connection

--- lib/spree_social/engine.rb
+++ lib/spree_social/engine.rb
@@ -37,7 +37,7 @@ module SpreeSocial
 
   # Setup all OAuth providers
   def self.init_provider(provider, scope='email')
-    return unless ActiveRecord::Base.connected?
+    return unless ActiveRecord::Base.connection_pool.with_connection { |con| con.active? }  rescue false
     return unless ActiveRecord::Base.connection.data_source_exists?('spree_authentication_methods')
     key, secret = nil
     Spree::AuthenticationMethod.where(environment: ::Rails.env).each do |auth_method|
