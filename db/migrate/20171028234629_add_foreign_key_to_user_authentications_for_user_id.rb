e371ce5286af4404baf43cd5261e132cea15d159
Laertis Pappas <laertis.pappas@gmail.com>
2017-10-29 03:09:49 +0300

    Adds FK for user_id in user_authentications

---
+++ db/migrate/20171028234629_add_foreign_key_to_user_authentications_for_user_id.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToUserAuthenticationsForUserId < ActiveRecord::Migration[4.2]
+  def change
+    add_foreign_key :spree_user_authentications, :spree_users, column: :user_id
+  end
+end
