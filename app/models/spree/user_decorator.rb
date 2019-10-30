3d569dc64e2b63ccdb77915d0cc1bd3c29807c97
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-30 09:05:59 +0100

    Changes class_eval to prepend in decorators

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,17 +1,23 @@
-Spree.user_class.class_eval do
-  has_many :user_authentications, dependent: :destroy
+module Spree
+  module UserDecorator
+    def self.prepended(base)
+      base.has_many :user_authentications, dependent: :destroy
 
-  devise :omniauthable
-
-  def apply_omniauth(omniauth)
-    skip_signup_providers = SpreeSocial::OAUTH_PROVIDERS.map { |p| p[1] if p[2] == 'true' }.compact
-    if skip_signup_providers.include? omniauth['provider']
-      self.email = omniauth['info']['email'] if email.blank?
+      base.devise :omniauthable
     end
-    user_authentications.build(provider: omniauth['provider'], uid: omniauth['uid'])
-  end
 
-  def password_required?
-    (user_authentications.empty? || !password.blank?) && super
+    def apply_omniauth(omniauth)
+      skip_signup_providers = SpreeSocial::OAUTH_PROVIDERS.map { |p| p[1] if p[2] == 'true' }.compact
+      if skip_signup_providers.include? omniauth['provider']
+        self.email = omniauth['info']['email'] if email.blank?
+      end
+      user_authentications.build(provider: omniauth['provider'], uid: omniauth['uid'])
+    end
+
+    def password_required?
+      (user_authentications.empty? || !password.blank?) && super
+    end
   end
 end
+
+Spree.user_class.prepend Spree::UserDecorator
